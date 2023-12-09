3e9ce4414bd497dbc07a62b8f36af84820414252
Dolbezhnik <krendelalenkin@gmail.com>
2023-12-09 15:40:51 +0300

    091223-1540

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+//alert('Hello');
+
+// const result = confirm("are you gay?");
+// console.log(result);
+
+// const answer = +prompt("Сколько времни в когда?", "вторник");
+// console.log(answer + 18);
+
+const answers = [];
+
+answers[0] = prompt('Имя любимае мае...', '');
+
+alert(`Hello, ${answers[0]}`)
+
+document.write(answers);
